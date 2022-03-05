7720f5b674a7987a686fd4515f8c2f3ae53d3602
Jayadeep Gaddam <gaddamjayadeep@gmail.com>
2022-03-05 23:10:25 +0530

    fixed class name

--- insertElement.java
+++ insertElement.java
@@ -1,33 +1,27 @@
-import java.lang.*;
 import java.util.*;
 
-public class Main {
+public class InsertElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr1 = new int[n];
-        for(int i = 0; i<=arr1.length - 1;i++)
-        {
+        for (int i = 0; i <= arr1.length - 1; i++) {
             arr1[i] = sc.nextInt();
         }
-		int idx = sc.nextInt();
-		int val = sc.nextInt();
-		int[] arr2 = new int[n+1];		
-		//arr[idx-1] = val;
-		for(int i = 0; i<=arr2.length - 1;i++)
-        {
-            for(int j=0;j<idx-1;j++)
-			{
-				arr2[j] = arr1[j];
-			}
-			arr2[idx-1] = val;
-			for(int k=idx;k<=arr2.length-1;k++)
-			{
-				arr2[k] = arr1[k-1];
-			}
+        int idx = sc.nextInt();
+        int val = sc.nextInt();
+        int[] arr2 = new int[n + 1];
+        // arr[idx-1] = val;
+        for (int i = 0; i <= arr2.length - 1; i++) {
+            for (int j = 0; j < idx - 1; j++) {
+                arr2[j] = arr1[j];
+            }
+            arr2[idx - 1] = val;
+            for (int k = idx; k <= arr2.length - 1; k++) {
+                arr2[k] = arr1[k - 1];
+            }
         }
-        for(int i = 0; i<=arr2.length - 1;i++)
-        {
+        for (int i = 0; i <= arr2.length - 1; i++) {
             System.out.print(arr2[i] + " ");
         }
     }
--- removeElement.java
+++ removeElement.java
@@ -1,7 +1,6 @@
-import java.lang.*;
 import java.util.*;
 
-public class Main {
+public class RemoveElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
