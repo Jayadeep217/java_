556f97b8f58100e311f297ec438080beabe86eb2
Jayadeep Gaddam <gaddamjayadeep@gmail.com>
2022-03-06 23:48:45 +0530

    add StarPatternII.java

---
+++ StarPatternII.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class StarPatternII {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int temp = n;
+        for(int i =1; i<=n;i++)
+        {
+            for(int j = 1;j<=temp;j++)
+            {
+                if(j == temp)
+                {
+                    System.out.print('*');
+                }
+                else if (j == 1)
+                {
+                    System.out.print('*');
+                }
+                else if ( i == 1)
+                {
+                    System.out.print('*');
+                }
+                else
+                {
+                    System.out.print(' ');
+                }
+            }
+            temp--;
+            System.out.println();
+        }
+        sc.close();
+    }
+}
