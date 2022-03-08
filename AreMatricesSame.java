fd787e98e47092d0669242216b18acda385ec96b
Jayadeep Gaddam <gaddamjayadeep@gmail.com>
2022-03-08 23:34:06 +0530

    Are Matrices Same ?

---
+++ AreMatricesSame.java
@@ -0,0 +1,14 @@
+public class AreMatricesSame {
+    public int solve(int[][] A, int[][] B) {
+        int equal = 1;
+        for(int i = 0; i<A.length;i++){
+            for(int j = 0; j<A[0].length;j++){
+                if(A[i][j] != B[i][j]){
+                    equal = 0;
+                    break;
+                }
+            }
+        }
+        return equal;
+    }
+}
