20613ef9d04c22c19131911c973043f6500a721f
Jayadeep Gaddam <gaddamjayadeep@gmail.com>
2022-03-08 23:59:21 +0530

    Matrix Transpose

---
+++ MatrixTranspose.java
@@ -0,0 +1,12 @@
+public class MatrixTranspose {
+    public int[][] solve(int[][] A) {
+        int[][] B = new int[A[0].length][A.length];
+        for(int i = 0;i<A[0].length;i++){
+            for(int j = 0;j<A.length;j++){
+                B[i][j] = A[j][i];
+            }
+        }
+        return B;
+    }
+    
+}
